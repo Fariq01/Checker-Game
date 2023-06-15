1e0b7386c01d560bff907730f276d2f0cb0b8593
fariq01 <fariqfadhlan101@gmail.com>
2023-06-15 17:00:58 +0700

    Update GameRunner

--- Checker/GameRunner.cs
+++ Checker/GameRunner.cs
@@ -1,105 +1,121 @@
 namespace Checker
 {
-    public class GameRunner
-    {
-         private Dictionary<IPlayer, List<IPiece>> _playerPieceSet;
-         private IBoard _board;
-         private Rules _rules;
-         private IPlayer _currentPlayer;
+	public class GameRunner
+	{
+		 private Dictionary<IPlayer, List<IPiece>> _playerPieceSet;
+		 private IBoard _board;
+		 private Rules _rules;
+		 private Position _positon;
+		 private IPlayer _currentPlayer;
 
 
 
-         public void CreateBoard(int row, int column)
-         {
-            int[,] matrix = new int[row,column];
-            _board = new Board(matrix);
-         }
+		 public void CreateBoard(int row, int column)
+		 {
+			int[,] matrix = new int[row,column];
+			_board = new Board(matrix);
+		 }
 
-         public void CreatePlayerPieceSet(IPlayer player1, IPlayer player2)
-         {
-            _playerPieceSet = new Dictionary<IPlayer, List<IPiece>>();
+		 public void InitializePieceOnBoard(IPlayer player1, IPlayer player2)
+		 {
+			_playerPieceSet = new Dictionary<IPlayer, List<IPiece>>();
 
-            
-            List<IPiece> player1Pieces = new List<IPiece>();
-            List<IPiece> player2Pieces = new List<IPiece>();
+			
+			List<IPiece> player1Pieces = new List<IPiece>();
+			List<IPiece> player2Pieces = new List<IPiece>();
 
-            for(int row = 1; row <= 3; row++)
-            {
-                for(int col = 1; col <= 8; col += 2)
-                {
-                    if(row % 2 != 0)
-                    {
-                        Position initialPosition = new Position(row, col);
-                        IPiece piece = new Piece(PieceType.BlackMen, initialPosition);
-                        player1Pieces.Add(piece);
-                        
-                    }
-                    else
-                    {
-                        Position initialPosition = new Position(row, col + 1);
-                        IPiece piece = new Piece(PieceType.BlackMen, initialPosition);
-                        player1Pieces.Add(piece);
-                    }
-                }
-        
-            }
+			for(int row = 1; row <= 3; row++)
+			{
+				for(int col = 1; col <= 8; col += 2)
+				{
+					if(row % 2 != 0)
+					{
+						Position initialPosition = new Position(row, col);
+						IPiece piece = new Piece(PieceType.BlackMen, initialPosition);
+						player1Pieces.Add(piece);
+						
+					}
+					else
+					{
+						Position initialPosition = new Position(row, col + 1);
+						IPiece piece = new Piece(PieceType.BlackMen, initialPosition);
+						player1Pieces.Add(piece);
+					}
+				}
+		
+			}
 
-            for(int row = 6; row <= 8; row++)
-            {
-                for(int col = 1; col <= 8; col += 2)
-                {
-                    if(row % 2 == 0)
-                    {
-                        Position initialPosition = new Position(row, col);
-                        IPiece piece = new Piece(PieceType.WhiteMen, initialPosition);
-                        player2Pieces.Add(piece);
-                        
-                    }
-                    else
-                    {
-                        Position initialPosition = new Position(row, col + 1);
-                        IPiece piece = new Piece(PieceType.WhiteMen, initialPosition);
-                        player2Pieces.Add(piece);
-                    }
-                }
-        
-            }
+			for(int row = 6; row <= 8; row++)
+			{
+				for(int col = 1; col <= 8; col += 2)
+				{
+					if(row % 2 == 0)
+					{
+						Position initialPosition = new Position(row, col);
+						IPiece piece = new Piece(PieceType.WhiteMen, initialPosition);
+						player2Pieces.Add(piece);
+						
+					}
+					else
+					{
+						Position initialPosition = new Position(row, col + 1);
+						IPiece piece = new Piece(PieceType.WhiteMen, initialPosition);
+						player2Pieces.Add(piece);
+					}
+				}
+		
+			}
 
-            _playerPieceSet.Add(player1, player1Pieces);
-            _playerPieceSet.Add(player2, player2Pieces);
-    
-         }
+			_playerPieceSet.Add(player1, player1Pieces);
+			_playerPieceSet.Add(player2, player2Pieces);
+	
+		 }
+		 
+		 
+		 
+		public IPiece CheckPiece(Position target)
+		{
+			IPiece pieceReturn = null;
+			
+			foreach(var piece in _playerPieceSet.Values)
+			
+			{
+				if(piece.Exists(piece => piece.GetPosition() == target))
+				
+				{
+					pieceReturn = (IPiece)piece; 
+					
+					
+				}
+			}
+			
+			return pieceReturn;
+			
+			
+		}
 
-         public void MovePiece(IPlayer currentPlayer, Position early, Position target)
-         {
-            if(isCaptur)
-            {
+		 public void MovePiece(Position early, Position target)
+		 {
+			
+			if(_rules.IsLegalMoves(target))
+			{
+				
+				if(_rules.IsOccupied(_playerPieceSet, target))
+				{
+					if(_rules.IsCaptureMove(_currentPlayer, _playerPieceSet, target))
+					{
+						
+					}
+					
+				}
+				
+			}
+		 }
+		 
+		 
+		 
+		
 
-            }
-         }
 
-        //  public void CapturePiece(IPlayer currentPlayer, Position position)
-        //  {
-        //     IPlayer opponentPlayer = _playerPieceSet.Keys.FirstOrDefault(player => player != currentPlayer);
-
-        //     if (opponentPlayer != null)
-        //     {
-        //         List<IPiece> opponentPieces = _playerPieceSet[opponentPlayer];
-        //         IPiece capturedPiece = opponentPieces.Find(piece => piece.GetPosition() == position);
-
-        //         if (capturedPiece != null)
-        //         {
-        //             opponentPieces.Remove(capturedPiece);
-        //             Console.WriteLine($"Capture piece at ({position}) !");
-
-                    
-        //         }
-        //         else
-        //         {
-        //             Console.WriteLine("No piece to capture at the position.");
-        //         }
-        //     }
-        //  }
-
-    }
+	}
 }
--- Checker/Rules.cs
+++ Checker/Rules.cs
@@ -1,38 +1,81 @@
 namespace Checker
 {
-    public class Rules
-    {
-        public bool IsLegalMoves(Position destination)
-        {
-            int row = destination.GetRow();
-            int column = destination.GetColumn();
+	public class Rules
+	{
+		public bool IsLegalMoves(Position target)
+		{
+			int row = target.GetRow();
+			int column = target.GetColumn();
 
-            if((row - column) % 2 == 0)
-            {
-                return true;
-            }
+			if((row - column) % 2 == 0)
+			{
+				return true;
+			}
 
-            return false;
-        }
+			return false;
+		}
+		
 
-        public bool IsOccupied(Dictionary<IPlayer, List<IPiece>> playerPieceSet, Position position)
-        {
-            foreach(var playerPieces in playerPieceSet.Values)
-            {
-                if(playerPieces.Exists(piece => piece.GetPosition() == position))
-                {
-                    return true;
-                }
-            }
+		public bool IsOccupied(Dictionary<IPlayer, List<IPiece>> playerPieceSet, Position position)
+		{
+			foreach(var playerPieces in playerPieceSet.Values)
+			{
+				if(playerPieces.Exists(piece => piece.GetPosition() == position))
+				{
+					return true;
+				}
+			}
 
-            return false;
-        }
+			return false;
+		}
 
-        public bool IsCaptureMove(Piece sourcePiece, Piece destinationPiece)
-        {
-       
-        }
-    
+		public bool IsCaptureMove(IPlayer currentPlayer, Dictionary<IPlayer, List<IPiece>> playerPieceSet, Position target)
+		{
+			IPlayer? opponentPlayer = null;
+			List<IPiece>? opponentPieces = null;
+			IPiece? capturedPiece = null;
+			
+			// cari opponentPlayer di dict playerpieceset
+			foreach(var player in playerPieceSet.Keys)
+			{
+				if(player != currentPlayer)
+				{
+					
+					opponentPlayer = player;
+				
+				}
+			}
+			// masukin opponentPieces
+			if(opponentPlayer != null)
+			
+			{
+				opponentPieces = playerPieceSet[opponentPlayer];
+			}
+			
+			//cari piece di opponentPieces yang berada di posisi target
+			foreach(var piece in opponentPieces)
+			{
+				if(piece.GetPosition() == target)
+				
+				{
+					capturedPiece = piece;
+				}
+			}
+			
+			if(capturedPiece != null)
+			
+			{
+				opponentPieces.Remove(capturedPiece);
+				//ganti pake display
+				Console.WriteLine($"{capturedPiece} is captured");	
+				
+				return true;
+							
+			}
+			
+			return false;
+		}
+	
 
-    }
+	}
 }
